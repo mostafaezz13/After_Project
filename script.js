1c05dd249ebb94e9418736296b57bf50ab34fee1
mostafaezz13 <157657442+mostafaezz13@users.noreply.github.com>
2024-12-24 17:43:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// let pr = document.querySelector(".cardat");
+
+// function right() {
+//   pr.scrollBy({
+//     left: 300,
+//     behavior: "smooth",
+//   });
+// }
+// function left() {
+//   pr.scrollBy({
+//     left: -300,
+//     behavior: "smooth",
+//   });
+// }
+
+import { Carousel, initMDB } from "mdb-ui-kit";
+
+initMDB({ Carousel });
